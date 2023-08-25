0f0a17ef67a949533c644de5caa20eaaaf86f270
Reza <reza.amini.b@gmail.com>
2023-08-25 11:22:18 +0100

    - added and send a sample notification to browser, using the javascript Notification API.

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const button = document.querySelector("button");
 
 button.addEventListener("click", () => {
-  // alert("hi");
+  button.addEventListener("click", () => {
+    // alert("hi");
+  
+    Notification.requestPermission().then((permission) => {
+      if (permission === "granted") {
+        const notification = new Notification("Sample Javascript Notification", {
+          body: Math.random(),
+        });
+      }
+  
+      notification.addEventListener("error", (e) => {});
+    });
+  });
 });
