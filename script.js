b9b68fec65ce89553f601c4dd31f82f830c7411a
Reza <reza.amini.b@gmail.com>
2023-08-26 10:52:24 +0100

    - added a possibility to send push notification... when the main page is hidden or when the user is not active in the main page.

--- script.js
+++ script.js
@@ -3,15 +3,41 @@ const button = document.querySelector("button");
 button.addEventListener("click", () => {
   button.addEventListener("click", () => {
     // alert("hi");
-  
+
     Notification.requestPermission().then((permission) => {
       if (permission === "granted") {
-        const notification = new Notification("Sample Javascript Notification", {
-          body: Math.random(),
-        });
+        const notification = new Notification(
+          "Sample Javascript Notification",
+          {
+            body: Math.random(),
+          }
+        );
       }
-  
+
       notification.addEventListener("error", (e) => {});
     });
   });
+});
+
+let outsideNotification;
+let interval;
+document.addEventListener("visibilitychange", () => {
+  if (document.visibilityState === "hidden") {
+    let goneDate = new Date();
+    interval = setInterval(() => {
+      outsideNotification = new Notification("Please come back ! ", {
+        body: `You have been gone for 
+        ${Math.round((new Date() - goneDate) / 1000)} seconds`,
+        tag: "Left the Page Tag",
+      });
+    }, 100);
+  } else {
+    if (interval) {
+      clearInterval();
+    }
+
+    if (outsideNotification) {
+      outsideNotification.close();
+    }
+  }
 });
