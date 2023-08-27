c436eeeeb39eb62d3534473db5b55211cebe6a77
Reza <reza.amini.b@gmail.com>
2023-08-27 11:39:26 +0100

    - added push notification icon and related ... file

--- script.js
+++ script.js
@@ -1,43 +1,42 @@
 const button = document.querySelector("button");
 
 button.addEventListener("click", () => {
-  button.addEventListener("click", () => {
-    // alert("hi");
+  // alert("hi");
 
-    Notification.requestPermission().then((permission) => {
-      if (permission === "granted") {
-        const notification = new Notification(
-          "Sample Javascript Notification",
-          {
-            body: Math.random(),
-          }
-        );
-      }
+  Notification.requestPermission().then((permission) => {
+    if (permission === "granted") {
+      const notification = new Notification("Sample Javascript Notification", {
+        body: Math.random(),
+        tag: "Test Unique Tag",
+        icon: "./bell.png",
+      });
+    }
 
-      notification.addEventListener("error", (e) => {});
-    });
+    // notification.addEventListener("error", (e) => {});
   });
 });
 
-let outsideNotification;
-let interval;
-document.addEventListener("visibilitychange", () => {
-  if (document.visibilityState === "hidden") {
-    let goneDate = new Date();
-    interval = setInterval(() => {
-      outsideNotification = new Notification("Please come back ! ", {
-        body: `You have been gone for 
-        ${Math.round((new Date() - goneDate) / 1000)} seconds`,
-        tag: "Left the Page Tag",
-      });
-    }, 100);
-  } else {
-    if (interval) {
-      clearInterval();
-    }
+// let outsideNotification;
+// let interval;
+// document.addEventListener("visibilitychange", () => {
+//   if (document.visibilityState === "hidden") {
+//     console.log("hidden");
+//     let goneDate = new Date();
+//     interval = setInterval(() => {
+//       outsideNotification = new Notification("Please come back ! ", {
+//         body: `You have been gone for
+//         ${Math.round((new Date() - goneDate) / 1000)} seconds`,
+//         tag: "Left the Page Tag",
+//         // icon: "./bell.png",
+//       });
+//     }, 100);
+//   } else {
+//     if (interval) {
+//       clearInterval();
+//     }
 
-    if (outsideNotification) {
-      outsideNotification.close();
-    }
-  }
-});
+//     if (outsideNotification) {
+//       outsideNotification.close();
+//     }
+//   }
+// });
